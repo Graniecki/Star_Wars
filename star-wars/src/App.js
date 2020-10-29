20766de679999d5b5c67769299048c44aa69083b
Ihor Hranetskyi <graniecki20@gmail.com>
2020-10-29 15:44:41 +0200

    Catalogue was created and added to App component

--- star-wars/src/App.js
+++ star-wars/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import { Catalogue } from './components/Catalogue';
 
 function App() {
   return (
     <div className="App">
-    
+      <Catalogue />
     </div>
   );
 }
---
+++ star-wars/src/components/Catalogue/Catalogue.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Catalogue.css';
+
+export const Catalogue = () => {
+  return (
+    
+  );
+};
